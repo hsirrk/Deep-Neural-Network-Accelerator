1e9a0c6cfa27b33945ca1ac55ffec4f8a3c08104
hsirrk <140936903+hsirrk@users.noreply.github.com>
2024-12-25 04:01:48 -0700

    Update dotopt.sv

--- task7/dotopt.sv
+++ task7/dotopt.sv
@@ -19,4 +19,143 @@ module dotopt(input logic clk, input logic rst_n,
 
     // your code: you may wish to start by copying code from your "dot" module, and then add control for master2_* port
 
+	 enum {RST, SLAVEINPUT, READ_WEIGHT_ACTIVATION, CALC_DOTPRODUCT, CALC_WAIT, DONE} state;
+	 logic [31:0] weightAds, activationAds, vecLength, i;
+	 logic signed [31:0] weight, actVec;
+	 logic signed [63:0] sumtemp, sum;
+	 
+	 
+	 always_ff @(posedge clk) begin
+		if (!rst_n) begin
+			state <= RST;
+		end
+		else begin
+			case(state)
+				RST: begin
+							state <= SLAVEINPUT;
+							
+							i <= 32'b0;
+							weightAds <= 32'h00000000;
+							activationAds <= 32'h00000000;
+							vecLength <= 32'd0;
+							sum <= 32'd0;
+							weight <= 32'd5;
+							actVec <= 32'd5;
+					  end
+				SLAVEINPUT: begin
+									//state <= READ_WEIGHT; //start copy when CPU write value to word offset 0
+									//i <= 32'b1;
+									
+									if (slave_write) begin
+										case(slave_address)
+											4'b0000: begin
+															state <= READ_WEIGHT_ACTIVATION; //start copy when CPU write value to word offset 0
+															i <= 32'd0;
+														end
+											4'd2: begin
+															//word offset 2, write destination byte address
+															weightAds <= slave_writedata;
+														end
+											4'd3: begin
+															//word offset 3, write source byte address
+															activationAds <= slave_writedata;
+														end
+											4'd5: begin
+															//word offset 3, num of 32-bit words to copy
+															vecLength <= slave_writedata;
+														end
+											default: begin
+															state <= SLAVEINPUT;
+														end
+										endcase
+									end
+								end
+				READ_WEIGHT_ACTIVATION: begin
+									if (!master_waitrequest && master_readdatavalid) begin
+										state <= CALC_DOTPRODUCT;
+										weight <= master_readdata;
+										actVec <= master2_readdata;
+									end
+								 end
+				CALC_DOTPRODUCT: begin
+											sum <= sum + sumtemp;
+												if ( (i+32'b1) == vecLength) begin
+														state <= DONE;
+												end
+												else begin
+													state <= CALC_WAIT;
+												end
+									  end
+				CALC_WAIT: begin
+											if (!master_waitrequest) begin
+													state <= READ_WEIGHT_ACTIVATION;
+													i <= i + 32'b1;
+											end
+										 end
+				DONE: begin
+							if (slave_address == 4'b0 && slave_read) begin
+								state <= SLAVEINPUT; //copy process finsihed when CPU read offset 0
+								i <= 32'b0;
+								weightAds <= 32'h00000000;
+								activationAds <= 32'h00000000;
+								vecLength <= 32'd0;
+								sum <= 32'd0;
+								weight <= 32'd5;
+								actVec <= 32'd5;
+							end
+						end
+				default: begin
+				
+							end
+			endcase
+		end
+	 end
+	 
+	 always_comb begin
+			master_writedata = 32'b0;
+			master_address = 32'b0;
+			master_write = 1'b0;
+			master_read = 1'b0;
+			slave_waitrequest = 1'b0;
+			slave_readdata = 32'b0;
+			sumtemp = 64'b0;
+			master2_read = 1'b0;
+			master2_address = 32'b0;
+			
+			case(state)
+				RST: begin
+				
+					  end
+				SLAVEINPUT: begin
+
+								end
+				READ_WEIGHT_ACTIVATION: begin
+										slave_waitrequest = 1'b1;
+										
+										//weight from SDRAM
+										master_address = weightAds + i*32'd4;
+										master_read = 1'b1;
+										
+										//INPUT from SRAM
+										master2_address = activationAds + i*32'd4;
+										master2_read = 1'b1;
+								 end
+				CALC_DOTPRODUCT: begin
+											slave_waitrequest = 1'b1;
+											sumtemp = weight*actVec;
+										end
+				CALC_WAIT: begin
+											slave_waitrequest = 1'b1;
+										 end
+				DONE: begin
+							if (slave_address == 4'b0 && slave_read) begin
+								slave_readdata = sum[47:16];
+							end
+						end
+				default: begin
+				
+							end
+			endcase
+	 end
+	 
 endmodule: dotopt
